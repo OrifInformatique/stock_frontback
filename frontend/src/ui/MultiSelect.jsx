0adc07668c8e5d216f9ab3074de63e8fbc04f643
Dervey Dylan <114241438+derveydylan@users.noreply.github.com>
2025-04-29 13:29:56 +0200

    fix: multiselect not having border

--- frontend/src/ui/MultiSelect.jsx
+++ frontend/src/ui/MultiSelect.jsx
@@ -52,7 +52,7 @@ const MultiSelect = ({
                 <div
                     onClick={() => { if(!isDisabled) setIsOpen((prev) => !prev) }}
                     className={clsx(
-                        "rounded-md w-full h-fit px-4 py-2 text-center select-none hover:cursor-pointer",
+                        "rounded-md w-full h-fit px-4 py-2 border border-gray-500 text-center select-none hover:cursor-pointer",
                         isDisabled ? "bg-stone-300 cursor-not-allowed" : "bg-background",
                         errors.length > 0 && "border-2 border-solid border-red-500",
                         className
