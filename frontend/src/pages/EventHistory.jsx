27f4df45b51d5e8fa5f89bfd867077f8c847190b
RoxBlox3 <RoxBlox33@proton.me>
2025-05-28 13:20:16 +0200

    Added the information of the exemplary and put a start of a button

--- frontend/src/pages/EventHistory.jsx
+++ frontend/src/pages/EventHistory.jsx
@@ -6,7 +6,7 @@ import Button from "../ui/Button.jsx";
 import Image from "../ui/Image.jsx";
 
 const EventHistory = () => {
-    const { t } = useTranslation("item");
+    const { t } = useTranslation("event");
     const [events, setEvents] = useState([]);
     const [info, setInfo] = useState({});
 
@@ -25,22 +25,27 @@ const EventHistory = () => {
 
     return (
         <>
-            <div className="flex justify-center flex-row">
+            <div className="flex justify-center flex-row mb-5">
                 <div className="flex flex-col content-center">
                     <h1 className="text-4xl text-center">
-                        Historique des évènements
+                        {t("event_history")}
                     </h1>
-                    <div className="p-3 bg-zinc-300">
+                    <Button label={t("return_to_item")} />
+                    {/* TODO: check if it's item or object, add onclick link, check other changes that can be made*/}
+                    <div className="p-3 bg-zinc-300 my-5 rounded-md flex flex-row">
                         <Image
                             src={`/images/${info.image_url}`}
                             alt={`Image of ${info.name}`}
                             size={150}
                         />
+                        <div className="ml-4 w-full flex flex-col justify-center">
+                            <p className="text-xl">{info.inventory_prefix}</p>
+                            <p>{info.name}</p>
+                        </div>
                     </div>
-                    <h2 className="text-2xl">Liste des évènements</h2>
+                    <h2 className="text-2xl text-center">{t("events_list")}</h2>
                 </div>
             </div>
-            {/* <Button label={"test"} />; */}
             <Event events={events} />
         </>
     );
